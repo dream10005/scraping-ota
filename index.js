0512193d7d41bf5167936ea018765966dd75e8ad
Dreamiiz <Dreamiiz@Dreamiizs-MacBook-Pro.local>
2017-12-28 21:24:13 +0700

    Update

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-// const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
 
-// let scrape = async () => {
-//     const browser = await puppeteer.launch({headless: false});
-//     const page = await browser.newPage();
+let scrape = async () => {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
 
-//     await page.goto('https://www.traveloka.com/th-th/hotel/search?spec=22-12-2017.23-12-2017.1.1.HOTEL_GEO.10000045.Bangkok.1');
+    await page.goto('https://www.agoda.com/pages/agoda/default/DestinationSearchResult.aspx?city=9395&checkIn=2017-12-28&los=1&rooms=1&adults=1&children=0&cid=-1&pagetypeid=1&origin=TH&tag=&gclid=&aid=130243&userId=9cd2ea20-775b-4692-94d6-f647642c664d&languageId=1&storefrontId=3&currencyCode=THB&htmlLanguage=en-us&trafficType=User&cultureInfoName=en-US&checkOut=2017-12-29&childages=&priceCur=THB&hotelReviewScore=5&tabId=1&issearchfromhomepage=true&ckuid=9cd2ea20-775b-4692-94d6-f647642c664d');    
+
+    const result = await page.evaluate(() => {
+        let data = []; // Create an empty array that will store our data
+        let elements = document.querySelectorAll('.price.dark-gray1'); // Select all Products
+        let names = document.querySelectorAll('h3.hotel-name span');
+        // for (var element of elements){ // Loop through each proudct
+        //     let title = element.innerText; // Select the title
+        //     let price = element.innerText; // Select the price
+
+        //     data.push({title, price}); // Push an object with the data onto our array
+        // }
 
 
-//     const result = await page.evaluate(() => {
-//         let data = []; // Create an empty array that will store our data
-//         let elements = document.querySelectorAll('.tvat-hotelName'); // Select all Products
+        // let a = elements[0].innerText;
+        // let b = names[0].innerText;
 
-//         for (var element of elements){ // Loop through each proudct
-//             let title = element.innerText; // Select the title
-//             let price = element.innerText; // Select the price
+        // data.push({a,b});
+        for (var i = 0; i <= 1; i++) {
+            let name = names[i].innerText;
+            let price = elements[i].innerText;
 
-//             data.push({title, price}); // Push an object with the data onto our array
-//         }
+            data.push({name,price});
+        }
+        return data; // Return our data array
+    });
 
-//         return data; // Return our data array
-//     });
+    browser.close();
+    return result; // Return the data
+};
 
-//     browser.close();
-//     return result; // Return the data
-// };
-
-// scrape().then((value) => {
-//     console.log(value); // Success!
-// });
-
-console.log('wtf');
+scrape().then((value) => {
+    console.log(value); // Success!
+});
